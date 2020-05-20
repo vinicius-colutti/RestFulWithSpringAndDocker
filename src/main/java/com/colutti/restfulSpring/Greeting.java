9480e28687bbac45a91bd8deb2ea441f4052bc3c
Vinicius Colutti <viniciuscolutti@hotmail.com>
2020-05-20 12:20:29 -0300

    first endpoint

---
+++ src/main/java/com/colutti/restfulSpring/Greeting.java
@@ -0,0 +1,20 @@
+package com.colutti.restfulSpring;
+
+public class Greeting {
+	
+	private final long id;
+	private final String content;
+	
+	public Greeting(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getContent() {
+		return content;
+	}
+	
+}
