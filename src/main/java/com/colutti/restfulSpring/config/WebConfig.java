89a23bd7f3951216142ab54f6a48298dbaf7862e
Vinicius Colutti <viniciuscolutti@hotmail.com>
2020-05-21 21:27:42 -0300

    Permitando a aplicação consumir e produzir YAML

--- src/main/java/com/colutti/restfulSpring/config/WebConfig.java
+++ src/main/java/com/colutti/restfulSpring/config/WebConfig.java
@@ -1,25 +1,37 @@
 package com.colutti.restfulSpring.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.colutti.restfulSpring.serialization.converter.YamlJackson2HttpMessageConverter;
 
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 	
+	private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
+	
+	
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
+	}
+	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		
-		/* VIA EXTENSÃO */
+		/* VIA EXTENSÃO
 		configurer.favorParameter(false)
 		.ignoreAcceptHeader(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
+		.mediaType("xml", MediaType.APPLICATION_XML);*/
 		
 		/*VIA QUERY PARAMETER
 		configurer.favorPathExtension(false)
@@ -39,7 +51,8 @@ public class WebConfig implements WebMvcConfigurer {
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
+		.mediaType("xml", MediaType.APPLICATION_XML)
+		.mediaType("x-yaml", MEDIA_TYPE_YML);
 	}
 
 }
--- src/main/java/com/colutti/restfulSpring/controllers/PersonController.java
+++ src/main/java/com/colutti/restfulSpring/controllers/PersonController.java
@@ -24,35 +24,35 @@ public class PersonController {
 	@Autowired
 	private PersonServices services;
 	
-	@GetMapping(produces = {"application/json", "application/xml"})
+	@GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
 	public List<PersonVO> findAll(){
 		
 		return services.findAll();
 		
 	}
 	
-	@GetMapping(value = "/{id}", produces = {"application/json", "application/xml"})
+	@GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
 	public PersonVO findById(@PathVariable("id") Long id){
 		
 		return services.findById(id);
 		
 	}
 	
-	@PostMapping(produces = {"application/json", "application/xml"}, consumes = {"application/json", "application/xml"})
+	@PostMapping(produces = {"application/json", "application/xml", "application/x-yaml"}, consumes = {"application/json", "application/xml", "application/x-yaml"})
 	public PersonVO create(@RequestBody PersonVO person){
 		
 		return services.create(person);
 		
 	}
 	
-	@PostMapping(value="/v2", produces = {"application/json", "application/xml"}, consumes = {"application/json", "application/xml"})
+	@PostMapping(value="/v2", produces = {"application/json", "application/xml", "application/x-yaml"}, consumes = {"application/json", "application/xml", "application/x-yaml"})
 	public PersonVOV2 createV2(@RequestBody PersonVOV2 person){
 		
 		return services.createV2(person);
 		
 	}
 	
-	@PutMapping(produces = {"application/json", "application/xml"}, consumes = {"application/json", "application/xml"})
+	@PutMapping(produces = {"application/json", "application/xml", "application/x-yaml"}, consumes = {"application/json", "application/xml", "application/x-yaml"})
 	public PersonVO update(@RequestBody PersonVO person){
 		
 		return services.update(person);
