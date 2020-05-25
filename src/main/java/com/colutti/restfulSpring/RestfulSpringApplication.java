8245e19b1bcea329a62e672317bd6c088d174fbc
Vinicius Colutti <viniciuscolutti@hotmail.com>
2020-05-25 18:07:10 -0300

    Adicionando configurações para arquivos e armazenamento

--- src/main/java/com/colutti/restfulSpring/RestfulSpringApplication.java
+++ src/main/java/com/colutti/restfulSpring/RestfulSpringApplication.java
@@ -3,9 +3,15 @@ package com.colutti.restfulSpring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.colutti.restfulSpring.config.FileStorageConfig;
+
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
 @EnableAutoConfiguration
 @ComponentScan
 public class RestfulSpringApplication {
---
+++ src/main/java/com/colutti/restfulSpring/config/FileStorageConfig.java
@@ -0,0 +1,18 @@
+package com.colutti.restfulSpring.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix= "file")
+public class FileStorageConfig {
+	
+	private String uploadDir;
+
+	public String getUploadDir() {
+		return uploadDir;
+	}
+
+	public void setUploadDir(String uploadDir) {
+		this.uploadDir = uploadDir;
+	}
+	
+}
